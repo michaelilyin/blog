0ba92d7d7b101058e19ea812cd0587d4855cb25e
Michael Ilyin <mr.ilyin.michael@gmail.com.com>
2016-03-27 19:02:02 +0300

    gitignore

--- webapp/target/vsu-demo-webapp/resources/filters/commonFilters.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    'use strict';
-
-    angular.module('webmodule').filter("sanitize", ['$sce', function($sce) {
-        return function(htmlCode){
-            return $sce.trustAsHtml(htmlCode);
-        }
-    }]);
-})();
