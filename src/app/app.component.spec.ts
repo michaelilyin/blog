75ceec0830e0dd1a6b907fa3c8c8c42afcb0de73
michaelilyin <michael.s.ilyin@gmail.com>
2017-08-20 13:32:52 +0300

    fix test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -11,6 +11,9 @@ import {AuthComponent} from './profile/auth/auth.component';
 import {AngularFireAuth} from 'angularfire2/auth';
 import {FirebaseApp} from 'angularfire2';
 import {AuthProviders, UserProfileService} from './profile/userprofile.service';
+import {TranslateModule, TranslateService} from '@ngx-translate/core';
+import {NgProgressModule} from 'ngx-progressbar';
+import {LanguageService} from './common/service/language.service';
 
 class ConfigurationServiceMock extends ConfigurationService {
     loadConfig() {
@@ -28,6 +31,10 @@ class UserProfileServiceMock extends UserProfileService {
     }
 }
 
+class LanguageServiceMock extends LanguageService {
+    initStaticTranslator(translateService: TranslateService) {    }
+}
+
 describe('AppComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -39,6 +46,7 @@ describe('AppComponent', () => {
                 { provide: APP_BASE_HREF, useValue: '/' },
                 { provide: ConfigurationService, useClass: ConfigurationServiceMock },
                 { provide: UserProfileService, useClass: UserProfileServiceMock },
+                { provide: LanguageService, useClass: LanguageServiceMock }
             ],
             imports: [
                 MdButtonModule,
@@ -46,7 +54,8 @@ describe('AppComponent', () => {
                 MdMenuModule,
                 MdDialogModule,
                 RouterModule.forRoot([{path: '', loadChildren: 'app/home/home.module#HomeModule'}]),
-                SlimLoadingBarModule.forRoot(),
+                NgProgressModule,
+                TranslateModule.forRoot(),
                 ToastModule.forRoot()
             ]
         }).compileComponents();
