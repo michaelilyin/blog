6930c6a0f00e015dfb2d4dfb77c36caa97aa0d98
michaelilyin <michael.s.ilyin@gmail.com>
2018-01-02 17:00:34 +0300

    forgotten

---
+++ src/app/common/dev-stub/dev-stub.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dev-stub',
+  templateUrl: './dev-stub.component.html',
+  styleUrls: ['./dev-stub.component.css']
+})
+export class DevStubComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
